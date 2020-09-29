bad6f1f97116fd6a94e5047cb43920dce6cde464
Sebastián Quintero Arias <60161237+SEB91QA@users.noreply.github.com>
2020-09-28 22:42:33 -0500

    New Exercises Chapter 4

---
+++ Capitulo4/ejercicio4_3dII.C
@@ -0,0 +1,32 @@
+
+
+#include<stdio.h>
+#include<math.h>
+
+
+
+int main()
+{
+
+   int x;
+
+   for( x = 1; x <= 20; x++){
+
+      printf("%d\t", x);
+
+      if((x%5) == 0){
+
+         printf("\n");
+
+      }
+
+   }
+
+   return 0;
+
+
+}
+
+
+
+
