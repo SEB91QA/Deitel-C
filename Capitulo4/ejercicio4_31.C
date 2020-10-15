1fc04581fb988da49eb6bcb4429891c2a589298a
Sebastián Quintero Arias <60161237+SEB91QA@users.noreply.github.com>
2020-10-14 23:32:44 -0500

    Exercise 4.31

--- Capitulo4/ejercicio4_31.C
+++ Capitulo4/ejercicio4_31.C
@@ -40,10 +40,38 @@ int main()
    }
 
 
+   for( int i = 0; i <= 3; i++)
+   {
+
+      for( int j = 0; j <= i; j++)
+      {
+
+         printf(" ");
+
+      }
+
+      
+      for( int j = 1; j <= (7 - 2*i); j++)
+      {
+
+         printf("*");
+
+      }
+
+      
+      for( int j = 0; j <= i; j++)
+      {
+
+         printf(" ");
+
+      }
 
    
+      printf("\n");
 
+   }
 
+  
 
    return 0;
 
