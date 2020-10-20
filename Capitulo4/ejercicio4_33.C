63afa0a16a7103932ff9e5162d6f2ab329841c2a
Sebastián Quintero Arias <60161237+SEB91QA@users.noreply.github.com>
2020-10-19 23:52:15 -0500

    Roman Numbers

--- Capitulo4/ejercicio4_33.C
+++ Capitulo4/ejercicio4_33.C
@@ -10,79 +10,554 @@
 int main()
 {
 
-   for( int i = 1; i <= 10; i++)
-   {
+   for( int n = 1; n <= 10; n++ )
+   { 
 
-
-      if( i <= 3)
+      switch(n)
       {
 
-         for( int j = 1; j <= i; j++)
-         {
+         case 1:     
+         {   
+            for( int i = 1; i <= 10; i++)
+            {
 
-            printf("I");
+               if( i <= 3)
+               {
 
-         }
+                  for( int j = 1; j <= i; j++)
+                  {
 
-         printf("\n");
+                     printf("I");
 
-      }
+                  }
 
+                  printf("\n");
 
-      if( i == 4 )
-      {
-
-         printf("IV");
-         printf("\n");
-         continue;
-
-      }
+               }
 
 
+               if( i == 4 )
+               {
 
+                  printf("IV");
+                  printf("\n");
+                  continue;
 
-      if( i == 5 ) 
-      {
+               }
 
-         printf("V");
-         printf("\n");
-         continue;
-  
-      }
+               if( i == 5 ) 
+               {
 
+               printf("V");
+               printf("\n");
+               continue;
 
-      if( i >= 6 && i <= 8 )
-      {
-         printf("V");
+               }
+
+               
+               if( i >= 6 && i <= 8 )
+               {
+                  printf("V");
          
-         for( int j = 1; j <= (i-5); j++ )
-         {
+                  for( int j = 1; j <= (i-5); j++ )
+                  {
 
-            printf("I");
+                     printf("I");
 
-         } 
+                  } 
    
-         printf("\n"); 
+                  printf("\n"); 
 
-      }
+               }
 
-      if( i == 9 )
-      {
+               if( i == 9 )
+               {
 
-         printf("IX");
-         printf("\n");
-         continue;
-      }
+                  printf("IX");
+                  printf("\n");
+                  continue;
+               }
 
-      if( i == 10 )
-      {
+               if( i == 10 )
+               {
 
-         printf("X");
-         printf("\n");
+                  printf("X");
+                  printf("\n");
 
-      }
-  
-   }
+               }
+            }  
+         }            
+         break;
+
+
+          case 2:
+          {
+             for( int i = 1; i <= 10; i++)
+             {
+
+                printf("X");
+
+                if( i <= 3)
+                {
+
+                   for( int j = 1; j <= i; j++)
+                   { 
+
+                      printf("I");
+
+                   }
+
+                   printf("\n");
+
+                }
+
+
+                if( i == 4 )
+                {
+
+                   printf("IV");
+                   printf("\n");
+                   continue;
+
+                }
+
+                if( i == 5 )
+                {
+
+                printf("V");
+                printf("\n");
+                continue;
+
+                }
+
+
+                if( i >= 6 && i <= 8 )
+                {
+                   printf("V");
+   
+                   for( int j = 1; j <= (i-5); j++ )
+                   {
+   
+                      printf("I");
+
+                   }
+
+                   printf("\n");
+
+                }
+
+                if( i == 9 )
+                {
+
+                   printf("IX");
+                   printf("\n");
+                   continue;
+                }
+
+                if( i == 10 )
+                {
+
+                   printf("X");
+                   printf("\n");
+
+                }           
+               
+             }     
+                     
+          }  
+          break;
+
+          case 3:
+          {
+
+             for( int i = 1; i <= 10; i++)
+             { 
+ 
+                printf("XX");
+
+                if( i <= 3)
+                {
+
+                   for( int j = 1; j <= i; j++)
+                   {
+
+                      printf("I");
+
+                   }
+
+                   printf("\n");
+
+                }
+
+
+                if( i == 4 )
+                {
+
+                   printf("IV");
+                   printf("\n");
+                   continue;
+
+                }
+
+                if( i == 5 )
+                {
+
+                   printf("V");
+                   printf("\n");
+                   continue;
+
+                }
+
+
+                if( i >= 6 && i <= 8 )
+                {
+                   printf("V");
+
+                   for( int j = 1; j <= (i-5); j++ )
+                   {
+
+                      printf("I");
+
+                   }
+
+                   printf("\n");
+
+                }
+
+                if( i == 9 )
+                {
+
+                   printf("IX");
+                   printf("\n");
+                   continue;
+                }
+
+                if( i == 10 )
+                {
+
+                   printf("X");
+                   printf("\n");
+
+                }
+
+             }  
+
+          }
+          break;
+
+          case 4:
+          {
+
+             for( int i = 1; i <= 9; i++)
+             {
+ 
+                printf("XXX");
+
+                if( i <= 3)
+                {
+
+                   for( int j = 1; j <= i; j++)
+                   {
+
+                      printf("I");
+
+                   }
+
+                   printf("\n");
+
+                }
+
+
+                if( i == 4 )
+                {
+
+                   printf("IV");
+                   printf("\n");
+                   continue;
+
+                }
+
+                if( i == 5 )
+                {
+
+                   printf("V");
+                   printf("\n");
+                   continue;
+
+                }
+
+
+                if( i >= 6 && i <= 8 )
+                {
+                   printf("V");
+
+                   for( int j = 1; j <= (i-5); j++ )
+                   {
+
+                      printf("I");
+
+                   }
+
+                   printf("\n");
+
+                }
+
+                if( i == 9 )
+                {
+
+                   printf("IX");
+                   printf("\n");
+                   continue;
+                }
+               
+                /*
+                if( i == 10 )
+                {
+                   break;
+                   //printf("XC");
+                   //printf("\n");
+
+                }
+                */
+
+             }
+
+          }
+          printf("XL");
+          printf("\n");
+          break;
+                
+          case 5:
+          {
+
+             for( int i = 1; i <= 9; i++)
+             {
+ 
+                printf("XL");
+
+                if( i <= 3)
+                {
+
+                   for( int j = 1; j <= i; j++)
+                   {
+
+                      printf("I");
+
+                   }
+
+                   printf("\n");
+
+                }
+
+
+                if( i == 4 )
+                {
+
+                   printf("IV");
+                   printf("\n");
+                   continue;
+
+                }
+
+
+                if( i == 5 )
+                {
+
+                   printf("V");
+                   printf("\n");
+                   continue;
+                }
+
+
+                if( i >= 6 && i <= 8 )
+                {
+                   printf("V");
+
+                   for( int j = 1; j <= (i-5); j++ )
+                   {
+
+                      printf("I");
+
+                   }
+
+                   printf("\n");
+
+                }
+
+                if( i == 9 )
+                {
+
+                   printf("IX");
+                   printf("\n");
+                   continue;
+                }
+
+
+             }
+
+          }
+          printf("L");
+          printf("\n");
+          break;
+
+          case 6:
+          {
+         
+             for( int i = 1; i <= 10; i++)
+             {
+ 
+                printf("L");
+
+                if( i <= 3)
+                {
+
+                   for( int j = 1; j <= i; j++)
+                   {
+
+                      printf("I");
+
+                   }
+
+                   printf("\n");
+
+                }
+
+
+                if( i == 4 )
+                {
+
+                   printf("IV");
+                   printf("\n");
+                   continue;
+
+                }
+
+                if( i == 5 )
+                {
+
+                   printf("V");
+                   printf("\n");
+                   continue;
+
+                }
+
+
+                if( i >= 6 && i <= 8 )
+                {
+                   printf("V");
+
+                   for( int j = 1; j <= (i-5); j++ )
+                   {
+
+                      printf("I");
+
+                   }
+                 
+                   printf("\n");
+
+                }
+
+                if( i == 9 )
+                {
+
+                   printf("IX");
+                   printf("\n");
+                   continue;
+                }
+
+                if( i == 10 )
+                {
+
+                   printf("X");
+                   printf("\n");
+
+                }
+
+
+             }
+
+          }
+          break;
+
+
+          case 7:
+          {
+
+             for( int i = 1; i <= 10; i++ )
+             {
+
+                printf("L");
+
+                if( i <= 3)
+                {
+
+                   for( int j = 1; j <= i; j++)
+                   {
+ 
+                      printf("I");
+
+                   }
+                   
+                   printf("\n");
+
+                }
+
+                if( i == 4 )
+                {
+
+                   printf("IV");
+                   printf("\n");
+                   continue;
+                }
+
+                
+
+                if( i == 5 )
+                {
+
+                   printf("V");
+                   printf("\n");
+                   continue;
+                }
+
+
+                if( i >= 6 && i <= 8)
+                {
+
+                   printf("V");
+
+                   for(int j = 1; j <= (i-5); j++)
+                   {
+                   
+                      printf("I");
+
+                   
+                   }
+ 
+                   printf("\n");
+
+                }
+
+                if( i == 9)
+                {
+
+                   printf("IV");
+                   printf("\n");
+
+                }
+
+
+             }
+
+          printf("X");
+          printf("\n");
+          break;
+          }   
+
+        }
+
+   }   
+
 
    return 0;
 
