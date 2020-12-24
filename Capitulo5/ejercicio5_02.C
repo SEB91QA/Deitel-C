cb292858920f75f4f34b1384452818bfbd0afd78
Sebastián Quintero Arias <60161237+SEB91QA@users.noreply.github.com>
2020-12-24 18:41:09 -0500

    Chapter 5

---
+++ Capitulo5/ejercicio5_02.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+int cubo( int y );                //Variable de alcance de prototipo de función
+                                  //Variable de alcance de bloque
+
+int main()                        //Variable de alcance de archivo
+{
+
+
+   int x;                         //Variable de alcance de función
+
+   for( x = 1; x <= 10; x++ )
+   {
+
+      printf("%d\n", cubo(x) );
+
+   }
+
+
+   return 0;
+
+}
+
+
+int cubo( int y )
+{
+
+
+   return y * y * y;
+
+}
+
---
+++ Capitulo5/ejercicio5_03.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+
+int main()
+{
+
+
+   float r = sqrt(900.0);
+
+   printf("%f", r);
+
+
+   return 0;
+
+
+}
+
+
+
