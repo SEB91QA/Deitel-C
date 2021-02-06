ed26f4169495b5a0fd35fe6c1404587cd6473bde
Sebastián Quintero Arias <60161237+SEB91QA@users.noreply.github.com>
2021-02-06 00:18:36 -0500

    Chapter 6

---
+++ Capitulo6/ejemplo6_8.C
@@ -0,0 +1,41 @@
+/* Cadena de caracteres */
+
+
+
+#include<stdio.h>
+
+
+int main(){
+
+
+   char cadena1[ 20 ];
+   char cadena2[] = "literal de cadena";
+   int i;
+
+
+   printf("Introduce una cadena: ");
+   scanf("%s", cadena1);
+
+   
+   printf("La cadena1 es: %s\ncadena2 es: %s\n"
+          "La cadena1 con espacios entre caracteres es: \n", cadena1, cadena2 );
+
+
+   for( i = 0; cadena1[ i ] != '\0'; i++ ){
+
+      printf("%c ", cadena1[ i ]);
+
+   }
+
+
+   printf("\n");
+
+
+   return 0;
+
+
+
+}
+
+
+
