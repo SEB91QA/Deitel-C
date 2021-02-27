90e45ea5260c47e3ea2f6f0b50cf716219d81417
Sebastián Quintero Arias <60161237+SEB91QA@users.noreply.github.com>
2021-02-27 00:58:25 -0500

    Chapter 6

---
+++ Capitulo6/ejemplo6_17.C
@@ -0,0 +1,29 @@
+/*Inicialización de arreglos multidimensionales*/
+
+
+#include<stdio.h>
+
+
+void despliegaArreglo( const int a[][ 3 ]);
+
+
+
+int main(){
+
+
+   int arreglo1[ 2 ][ 3 ] = { { 1, 2, 3 }, {4, 5, 6 }};
+   int arreglo2[ 2 ][ 3 ] = { 1, 2, 3, 4, 5 };
+   int arreglo3[ 2 ][ 3 ] = { { 1, 2 }, { 4 }};
+
+
+   
+
+
+
+
+
+   return 0;
+
+}
+
+
